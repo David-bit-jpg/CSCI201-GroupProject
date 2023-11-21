a1715dae629152a2a37faa6a55b7958401b45eb1
Jiale Wang <67268612+David-bit-jpg@users.noreply.github.com>
2023-11-20 18:45:47 -0800

    Add files via upload

---
+++ backend/AddUser/addContactController.js
@@ -0,0 +1,52 @@
+// Import the database connection module
+const db = require('../db');
+
+function addFriend(userId, friendUserId, callback) {
+    // Check if the user exists
+    const checkUserQuery = `SELECT COUNT(*) AS userCount FROM User WHERE userID='${userId}'`;
+
+    db.performQuery(checkUserQuery, (error, userResult) => {
+        if (error) {
+            console.error('Error checking user existence:', error);
+            return callback(false);
+        }
+
+        if (userResult[0].userCount > 0) {
+            // Check if the friendUserID is already in the Friend table
+            const checkFriendQuery = `SELECT COUNT(*) AS friendCount FROM Friend WHERE friendUserID='${friendUserId}'`;
+
+            db.performQuery(checkFriendQuery, (error, friendResult) => {
+                if (error) {
+                    console.error('Error checking friend existence:', error);
+                    return callback(false);
+                }
+
+                if (friendResult[0].friendCount > 0) {
+                    return callback(false); // User is already a friend
+                }
+
+                // Add the friend to the Friend table
+                const addFriendQuery = `INSERT INTO Friend (userID, friendUserID, friendDate) VALUES ('${userId}', '${friendUserId}', NOW())`;
+
+                db.performQuery(addFriendQuery, (error, insertResult) => {
+                    if (error) {
+                        console.error('Error adding friend:', error);
+                        return callback(false);
+                    }
+
+                    return callback(insertResult.affectedRows > 0);
+                });
+            });
+        } else {
+            return callback(false); // User does not exist
+        }
+    });
+}
+
+// Example usage
+db.connectToDatabase(); // Connect to the database
+
+addFriend('userId1', 'friendUserId1', (result) => {
+    console.log('Friend added:', result);
+    db.closeConnection(); // Close the database connection after usage
+});
